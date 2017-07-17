301a5423eeec1e9f8484a5eca37ba761f9bc8a79
TGLIDE <thomglopes@gmail.com>
2017-07-17 15:36:22 -0300

    Update main.py
    -Update to get all images from an imgur album.
    -Update to how urls are written on wallpapers_downloaded.txt

--- src/main.py
+++ src/main.py
@@ -10,10 +10,11 @@ import urllib
 import time
 import sys
 import PIL
+import re
 
 from PIL import Image
 
-# Functions 
+
 def is_image(some_url):
     """Defines if a url is an image"""
     if some_url[-4:] != '.jpg' and some_url[-4:] != '.png':
@@ -49,7 +50,23 @@ def resize_images(files):
             the_img.resize((int(1366), int(((1366 * int(the_img.size[1])) / int(the_img.size[0])))), Image.ANTIALIAS)
         the_img.save(img)
 
+
+def is_imgur_album(link):
+    """Checks if link is from an imgur album"""
+    return "imgur.com/a/" in link
+
+
+def get_imgur_album_link(link):
+    """Gets all image links from an imgur album"""
+    page_source = urllib.request.urlopen(link)
+    page_source = str(page_source.read())
+    # the_match = re.search('<link rel="image_src"\s*href="([^">]*)"/>', page_source)
+    the_matches = re.findall('<a href="([^">]*)" class="zoom">', page_source)
+    return the_matches
+
+
 # Make a file_name and a list holding the wallpapers that were already downloaded, as to avoid repetition.
+# Also makes a error_log and a list for handling_errors
 file_name = 'wallpapers_downloaded.txt'
 if not os.path.isfile(file_name):
     wallpapers_downloaded = []
@@ -58,8 +75,6 @@ else:
         wallpapers_downloaded = f.read()
         wallpapers_downloaded = wallpapers_downloaded.split("\n")
         wallpapers_downloaded = list(filter(None, wallpapers_downloaded))
-
-# Makes a error_log and a list for handling_errors
 error_file = 'error_log.txt'
 errors_log = []
 
@@ -73,27 +88,53 @@ d_m_y = "{0}.{1}.{2}".format(current_date.day, current_date.month, current_date.
 save_folder = os.getcwd() + "\data\\" + "wallpapers\\" + d_m_y + "\\"
 if not os.path.exists(save_folder):
     os.makedirs(save_folder)
-    
+
 # Download wallpapers from hot posts of the day
 print("Fetching wallpapers...")
 new_wp_count = 0
 error_count = 0
 for submission in subreddit.hot(limit=20):
     if submission.url not in wallpapers_downloaded:
-        wallpapers_downloaded.append(submission.url)
-        if is_image(submission.url):     # Does it end in png/jpg or not? If yes: continue, else: put a .png at the end.
-            the_url = submission.url
+        # Check if it's a imgur album link
+        if is_imgur_album(submission.url):
+            the_urls = get_imgur_album_link(submission.url)
+            wallpapers_downloaded.append(submission.url)
+            for the_url in the_urls:
+                the_url = "http:{0}".format(the_url)
+                # Try downloading
+                try:
+                    urllib.request.urlretrieve(the_url, save_folder + str(new_wp_count + 1) + the_url[-4:])
+                    wallpapers_downloaded.append("{0} - {1}:".format(d_m_y, str(new_wp_count + 1)))
+                    wallpapers_downloaded.append(the_url)
+                    new_wp_count += 1
+                    print("{0} downloaded...".format(the_url))
+                # If it fails, put an error in the error log
+                except (urllib.error.HTTPError, urllib.error.URLError) as e:
+                    the_error = "{1} - {0}".format(the_url, e)
+                    print(the_error)
+                    errors_log.append(the_error + " - {0}".format(d_m_y))
+                    continue
+        # If it's not
         else:
-            the_url = submission.url + ".png"
-        try:    # Tries to download, catches forbidden or invalid link errors
-            urllib.request.urlretrieve(the_url, save_folder + str(submission.id) + the_url[-4:])
-        except (urllib.error.HTTPError, urllib.error.URLError) as e:
-            the_error = "{1} - {0}".format(the_url, e)
-            print(the_error)
-            errors_log.append(the_error + " - {0}".format(d_m_y))
-            continue
-        new_wp_count += 1
-        print("{0} downloaded...".format(the_url))
+            # Does it end in png/jpg or not? If yes: continue, else: put a .png at the end.
+            if is_image(submission.url):
+                the_url = submission.url
+            else:
+                the_url = submission.url + ".png"
+            # Try downloading
+            try:
+                urllib.request.urlretrieve(the_url, save_folder + str(new_wp_count + 1) + the_url[-4:])
+                wallpapers_downloaded.append("{0} - {1}:".format(d_m_y, str(new_wp_count + 1)))
+                wallpapers_downloaded.append(submission.url)
+                new_wp_count += 1
+                print("{0} downloaded...".format(the_url))
+            # If it fails, put an error in the error log
+            except (urllib.error.HTTPError, urllib.error.URLError) as e:
+                the_error = "{1} - {0}".format(the_url, e)
+                print(the_error)
+                errors_log.append(the_error + " - {0}".format(d_m_y))
+                continue
+
 print("Downloaded {0} new wallpapers!".format(new_wp_count))
 
 # Write wallpapers saved to avoid repetition
@@ -107,12 +148,8 @@ with open(error_file, 'a') as f:
 # Apply random wallpaper from current day folder
 all_files = glob.glob(save_folder + "\\*.png")
 all_files.extend(glob.glob(save_folder + "\\*.jpg"))
-
-# Resizes image to current desktop res
 print("Resizing images...")
 resize_images(all_files)
-
-# Randomly applies wallpaper to desktop
 all_files = glob.glob(save_folder + "\\*.png")
 all_files.extend(glob.glob(save_folder + "\\*.jpg"))
 rng = random.Random()
